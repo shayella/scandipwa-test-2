3a880055b701648afbab5a466e35b87edb5f4bf4
shayella <shayellae@gmail.com>
2022-05-06 01:21:34 +0300

    Code clean up

--- src/Progressbar.component.js
+++ src/Progressbar.component.js
@@ -4,17 +4,30 @@ import React,{Component} from 'react'
 class ProgressBar extends Component{
     render(){
         const {steps, selectedStep} = this.props
-        const allSteps = Object.keys(steps)
+        const stepsArr = Object.keys(steps).map((key) => [(key), steps[key]]);
+        const selectedStepInArray = stepsArr.filter(step=>step[0]==selectedStep)[0];
+        const indexofCurrentStep = stepsArr.indexOf(selectedStepInArray);
+
         
         return(
             <div className='Checkout-ProgressBar'>
-                {allSteps.map((step,i)=>{return(
-                    <div className='Step-wrapper'>
+                {
+                    stepsArr.map(step=>{
+                        let isFilled = stepsArr.indexOf(step) <= indexofCurrentStep;
+                        let isLastStep = stepsArr.indexOf(step) == stepsArr.length-1;
+                        
+                        return(
+                            <div key={step[1]["title"]["value"]} className='Step-wrapper'>
+                               <div className={ isFilled?"Step-line Step-line-selected":'Step-line'}></div>
+                                <div className={isLastStep?"Step-container-last":'Step-container'}>
+                                    <span className={isFilled?"Step-number Step-number-selected":'Step-number'}>{stepsArr.indexOf(step)+1}</span>
+                                     <p className='Step-title'>{step[1]["title"]["value"]}</p></div>
+                            </div>
+                        )
 
-                        <div className={selectedStep == step?"Step-line Step-line-selected":'Step-line'}></div>
-                        <div className={i+1==allSteps.length?"Step-container-last":'Step-container'}><span className={selectedStep==step?"Step-number Step-number-selected":'Step-number'} key={i}>{i+1}</span> <p className='Step-title'>{steps[step]["title"]["value"]}</p></div>
-                    </div>
-                )})}
+                        
+                    })
+                }
             </div>
         )
     }

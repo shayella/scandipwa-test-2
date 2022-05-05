5e4193a5410730955d53475d03901ad6314a3969
shayella <shayellae@gmail.com>
2022-05-06 02:43:58 +0300

    Made progress bar its own folder

---
+++ src/ProgressBar/Progressbar.component.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react'
+import "./Progressbar.styles.scss";
+
+
+class ProgressBar extends Component{
+    render(){
+        const {steps, selectedStep} = this.props
+        const stepsArr = Object.keys(steps).map((key) => [(key), steps[key]]);
+        const selectedStepInArray = stepsArr.filter(step=>step[0]==selectedStep)[0];
+        const indexofCurrentStep = stepsArr.indexOf(selectedStepInArray);
+
+        
+        return(
+            <div className='Progressbar-wrapper Progressbar'>
+                {
+                    stepsArr.map(step=>{
+                        let isFilled = stepsArr.indexOf(step) <= indexofCurrentStep;
+                        let isLastStep = stepsArr.indexOf(step) == stepsArr.length-1;
+                        
+                        return(
+                        <>
+                            <div  key={step[1]["title"]["value"]} className='Progressbar-container'>
+                                <div className={ isFilled?"Progressbar-filled":''}></div>
+                            </div>
+
+                            <div className={isLastStep?"Step-container-last":'Step-container'}>
+                                <span className={isFilled?"Step-number Step-number-selected":'Step-number'}>{stepsArr.indexOf(step)+1}</span>
+                                <p className='Step-title'>{step[1]["title"]["value"]}</p>
+                            </div>
+                            
+                         </>
+                                
+                        )
+
+                        
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default ProgressBar;
